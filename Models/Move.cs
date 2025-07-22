6880fd4f3ba1cb7123f4b81d8c7acc28b89c742b
Jaideep D Naik <jaideep.d@juegostudioz.com>
2025-07-22 12:04:30 +0530

    Add JSON property names to Move model for serialization

--- Models/Move.cs
+++ Models/Move.cs
@@ -1,6 +1,13 @@
+using System.Text.Json.Serialization;
+
 public class Move
 {
+    [JsonPropertyName("row")]
     public int Row { get; set; }
+
+    [JsonPropertyName("column")]
     public int Column { get; set; }
+
+    [JsonPropertyName("player")]
     public required string Player { get; set; } // "X" or "O"
 }
